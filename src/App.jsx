8534d2e2f148c29858e8627b25aa860981b30562
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-11 08:28:50 +0200

    [UPD] FORM CREATO CON AGGIUNTA DEL CAMPO PER L'AGGIUNTA DI UN NUOVO ARTICOLO

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const articles = [
   "X consigli per allenarti in sicurezza a casa",
   "X eventi sportivi da non perdere nel 2023",
-  "X modi per allenarti insieme al tuo can",
+  "X modi per allenarti insieme al tuo cane",
   "Come calcolare le calorie consumate",
   "5 app per monitorare i tuoi allenamenti",
   "Perché e quando farsi seguire da un personal trainer"
@@ -14,11 +14,17 @@ function App() {
     <>
       <div className="container">
         <h1>Articoli</h1>
-        <ul className="list-group">
+        <ul className="list-group mb-4">
           {articles.map((article, index) => (
             <li key={index} className="list-group-item">{article}</li>
           ))}
         </ul>
+        <form>
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Nuovo articolo" />
+            <button className="btn btn-primary">Aggiungi articolo</button>
+          </div>
+        </form>
       </div>
     </>
   );
