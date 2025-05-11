fb4d60a0155286fca8d60110f666a6deb514cfd1
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-11 08:21:26 +0200

    [UPD] MAP APPLICATO

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const article = [
+const articles = [
   "X consigli per allenarti in sicurezza a casa",
   "X eventi sportivi da non perdere nel 2023",
   "X modi per allenarti insieme al tuo can",
@@ -12,9 +12,16 @@ const article = [
 function App() {
   return (
     <>
-
+      <div className="container">
+        <h1>Articoli</h1>
+        <ul className="list-group">
+          {articles.map((article, index) => (
+            <li key={index} className="list-group-item">{article}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
-};
+}
 
 export default App;
