4e1177b0ab959fc91ec2833983a19f15a88d079e
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-11 08:02:34 +0200

    [UPD] ARRAY CREATO

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const article = [
+  "X consigli per allenarti in sicurezza a casa",
+  "X eventi sportivi da non perdere nel 2023",
+  "X modi per allenarti insieme al tuo can",
+  "Come calcolare le calorie consumate",
+  "5 app per monitorare i tuoi allenamenti",
+  "Perché e quando farsi seguire da un personal trainer"
+];
 
 function App() {
-  
+  return (
+    <>
+
+    </>
+  );
 };
 
 export default App;
